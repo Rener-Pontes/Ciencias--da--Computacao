f6d90e2532592a4ae905297464b9d26fdcaab5b9
AnimesR-Kun <79544467+AnimesR-Kun@users.noreply.github.com>
2022-06-18 21:14:28 -0300

    Create Problema_1.c

---
+++ 1-Periodo/Log-Prog/Atividades/Lista-1/Problema_1.c
@@ -0,0 +1,9 @@
+//Faça um programa que peça dois números inteiros, imprima a soma desses dois números na tela
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    printf("Soma: %d\n", num1 + num2);
+}
