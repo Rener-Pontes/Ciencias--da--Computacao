b319f03c7d8928bd40318bf2e16faf1612bd30bd
AnimesR-Kun <79544467+AnimesR-Kun@users.noreply.github.com>
2022-06-18 21:22:21 -0300

    Create Problema_2.c

---
+++ 1-Periodo/Log-Prog/Atividades/Lista-1/Problema_2.c
@@ -0,0 +1,10 @@
+//Escreva um valor que lê um valor em metros e exiba convertido em milímetros.
+
+#include <stdio.h>
+
+int main(){
+    int metros;
+    
+    scanf("%d", &metros);
+    printf("%d milimetros", metros * 1000);
+}
